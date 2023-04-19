4bc0b7e73c611304f7e54ca33da986fbffb3d792
unknown <idowu98official@gmail.com>
2023-04-19 11:17:43 +0100

    toggle switch tweaks

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -21,7 +21,7 @@ export default function DarkModeSwitch({}: Props) {
   }, [enabled]);
 
   return (
-    <div className="flex gap-2 mt-4">
+    <div className="flex items-center gap-2 mt-4">
       <div className="flex items-center order-2 gap-3">
         {enabled ? (
           <button onClick={() => setTheme("dark")}>
@@ -49,14 +49,14 @@ export default function DarkModeSwitch({}: Props) {
         checked={enabled}
         // @ts-ignore
         onChange={() => setEnabled(prev => !prev)}
-        className={`${enabled ? "bg-zinc-800" : "bg-zinc-500"}
-          relative inline-flex h-[30px] w-[66px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        className={`${enabled ? "bg-zinc-800" : "bg-dashboard-active"}
+          relative inline-flex h-[20px] w-[45px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Use setting</span>
         <span
           aria-hidden="true"
-          className={`${enabled ? "translate-x-9 bg-white" : "translate-x-0 bg-zinc-800"}
-            pointer-events-none inline-block h-[26px] w-[26px] transform rounded-full  shadow-lg ring-0 transition duration-200 ease-in-out`}
+          className={`${enabled ? "translate-x-6 bg-white" : "translate-x-0 bg-white"}
+            pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full  shadow-lg ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
     </div>
